a3432c0cb1b1468156e52e2b48e43a0594276299
maismuka <ciscoluqman@gmail.com>
2020-06-02 13:59:29 +0800

    Update heartbeatfirststartup.sh

--- heartbeatfirststartup.sh
+++ heartbeatfirststartup.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 tar -xvzf heartbeat.tar.gz
-cd heartbeat
+mv -f heartbeat.yml heartbeat-7.7.0-darwin-x86_64/filebeat.yml
+cd heartbeat-7.7.0-darwin-x86_64
 echo "
 #! /bin/bash
 heartbeat -e" > autostart.sh
