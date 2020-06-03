b2788bb07272de047d1905796a39dc3e5199d59f
maismuka <ciscoluqman@gmail.com>
2020-06-04 00:40:39 +0800

    Delete heartbeatfirststartup.sh

--- heartbeatfirststartup.sh
+++
@@ -1,8 +0,0 @@
-cd ~/Desktop/macOS-heartbeat/
-tar -xvzf heartbeat.tar.gz
-echo ./heartbeat -e > autostart.sh
-chmod +x autostart.sh
-cp -f heartbeat.yml heartbeat-7.7.0-darwin-x86_64/heartbeat.yml
-cp -f autostart.sh heartbeat-7.7.0-darwin-x86_64/autostart.sh
-cd ~/Desktop/macOS-heartbeat/heartbeat-7.7.0-darwin-x86_64/
-bash autostart.sh
